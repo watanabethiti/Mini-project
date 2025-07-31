6e007062ea18a61c8d1c361097af6626cd387ce0
watanabethiti <thitiporn.srir@ku.th>
2025-07-31 19:52:51 +0700

    Add files via upload

---
+++ Lib/ProductCatalog.java
@@ -0,0 +1,14 @@
+package Lib;
+import java.util.ArrayList;
+
+/**
+ * คลาสทำหน้าที่เป็นแคตาล็อกสินค้า (Repository)
+ */
+public class ProductCatalog {
+    private ArrayList<Product> products = new ArrayList<>();
+
+    //RI: products list is not null, contains no null elements, :
+    //AF: AF(products) = A catalog of all available products.
+
+    private void 
+}
